170bcc53713516ea939da205b25cd474b5000b48
samyak0604 <121559058+samyak0604@users.noreply.github.com>
2023-08-25 02:14:27 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -32,6 +32,7 @@ class Wrestler (Robot):
         time_step = int(self.getBasicTimeStep())
         while self.step(time_step) != -1:  # mandatory function to make the simulation run
             motion_library.play('Stand')
+            motion_library.play('Forward')
 
 
 
